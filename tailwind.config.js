4d12953f3d1de259e2b394a5840fd426c4667f99
Elam Vera Castro <elanvera@hotmail.com>
2023-09-11 18:56:47 -0500

    maquetando con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url('../image/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../image/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../image/yosemite.jpg')",
+        'LA': "url('../image/LA.jpg')",
+        'seattle': "url('../image/seattle.jpg')",
+        'new_york': "url('../image/new_york.jpg')",
+        'norway': "url('../image/image/norway.jpg')",
+        'sydney': "url('../image/sydney.jpg')",
+        'miami': "url('../image/miami.jpg')",
+        'switzerland': "url('../image/switzerland.jpg')",
+        'bali': "url('../image/bali.jpg')",
+        'norway': "url('../image/norway.jpg')",
+        'chicago': "url('../image/chicago.jpg')",
+        'europe': "url('../image/europe.jpg')",
+        'iceland': "url('../image/iceland.jpg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: {
+        Monserrat:['Monserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
